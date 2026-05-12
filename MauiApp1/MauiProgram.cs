a9e9ab46e20884cc6d395aeaca0df5b025c9f570
Gaukhar <g.marat24@apec.edu.kz>
2026-05-12 15:33:08 +0500

    Добавьте файлы проекта.

---
+++ MauiApp1/MauiProgram.cs
@@ -0,0 +1,17 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+    private int _checkCount = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCheckClicked(object sender, EventArgs e)
+    {
+        _checkCount++;
+        StatusLabel.Text = $"Button clicked: {_checkCount} times";
+    }
+}
